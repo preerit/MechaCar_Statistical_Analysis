9b56c751370f3fc4e0dcb7a86b042d6143402ded
preerit <105027698+preerit@users.noreply.github.com>
2022-08-26 13:16:45 +0530

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,8 +5,8 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacars_table)) #generate summary statistics
 
 suspension_coils <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-summarize_suspension <- suspension_coils %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
-summarize_suspension_by_lot <- suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+total_summary <- suspension_coils %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
 
 t.test(x = suspension_coils$PSI,mu=1500) #Null: Mean is 1500
 lot1_PSI = subset(suspension_coils, Manufacturing_Lot == "Lot1")
